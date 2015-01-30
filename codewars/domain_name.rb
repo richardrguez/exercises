e770998b66598d889dcf4d25bf9eebc7ad5e1554
richardrguez <richardrguez@users.noreply.github.com>
2015-01-29 22:15:33 -0400

    Create domain_name.rb

---
+++ codewars/domain_name.rb
@@ -0,0 +1,10 @@
+#Write a function that when given a URL as a string, parses out just the domain name and returns it as a string. For example:
+#domain_name("http://github.com/carbonfive/raygun") == "github" 
+#domain_name("http://www.zombie-bites.com") == "zombie-bites"
+#domain_name("https://www.cnet.com") == "cnet"
+
+#code
+def domain_name(url)
+    url.scan(/\w+[\-\w]+/).reject {|w| w =~ /(www|com|http)/}[0]
+end
+
