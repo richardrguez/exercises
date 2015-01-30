3ad181793aa3056b3fd28b8910764fa9051e63f6
richardrguez <richardrguez@users.noreply.github.com>
2015-01-29 21:33:58 -0400

    Create SwapCase.rb

---
+++ coderbyte/easy/SwapCase.rb
@@ -0,0 +1,15 @@
+# Using the Ruby language, have the function SwapCase(str) take the str parameter and swap the case of each character. 
+# For example: if str is "Hello World" the output should be hELLO wORLD. Let numbers and symbols stay the way they are.
+
+# Use the Parameter Testing feature in the box below to test your code with different arguments.      
+
+def SwapCase(str)
+
+  # code goes here
+  return str.swapcase 
+         
+end
+   
+# keep this function call here 
+# to see how to enter arguments in Ruby scroll down   
+SwapCase(STDIN.gets)    
